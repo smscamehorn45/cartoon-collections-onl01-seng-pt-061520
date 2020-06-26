5fb9479026de66f3ce86cab4c426fe7fd4b9e57b
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-26 03:16:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,13 @@
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+call_screams = ["bo", "bam", "boom", "bazam", "powzer"]
 
 
 def roll_call_dwarves(array)
+  i=0
+  while i < array.length
+  puts "#{i + 1}. #{array[i]}"
+  i += 1 
   
 end
 
